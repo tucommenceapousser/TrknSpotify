5013db7f3f0c941bd4a351c2512afb2ec45912c9
trhacknon anon <trhacknon@users.noreply.huggingface.co>
2023-05-14 22:17:22 +0000

    Update env_variables.py

--- env_variables.py
+++ env_variables.py
@@ -7,7 +7,7 @@ Created on Sat Jul 23 10:38:49 2022
 """
 from datetime import datetime 
 import os
-os.environ['SPOTIPY_CLIENT_ID'] = "fc99466410414fbb9aaafbfe76ab5a1d"
-os.environ['SPOTIPY_CLIENT_SECRET'] = "83577093a17d4db3989a074b85b7905a"
-os.environ['SPOTIPY_REDIRECT_URI'] = "http://www.google.com:1234"
+os.environ['SPOTIPY_CLIENT_ID'] = "2699193a7c0c4616928622c64079277f"
+os.environ['SPOTIPY_CLIENT_SECRET'] = "97ab376b6826430a994e35b5b5349a4f"
+os.environ['SPOTIPY_REDIRECT_URI'] = "http://www.google.com"
 #os.environ['SPOTIPY_REDIRECT_URI'] = "https://halimbox-spotify-shuffler-main-ss-p7ac1f.streamlitapp.com:1234"
